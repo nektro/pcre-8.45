3c444040e8d025f6d6c6234e79f57f9d2ae37c5c
Luna <git@l4.pm>
2024-06-30 18:42:25 -0300

    install pcre.h

--- build.zig
+++ build.zig
@@ -14,6 +14,7 @@ pub fn build(b: *std.Build) void {
     lib.addIncludePath(.{ .path = b.pathFromRoot(".") });
     lib.addIncludePath(copyFiles.getDirectory());
     lib.addIncludePath(std.Build.LazyPath{ .cwd_relative = "/usr/include" }); // temporary hack
+    lib.installHeader(b.path("pcre.h"), "pcre.h");
 
     lib.addCSourceFiles(.{
         .flags = &[_][]const u8{
